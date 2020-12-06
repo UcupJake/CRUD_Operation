691a3586e4aeaeb06894e2fdd3a11993f1e8e0a7
YusufJake <yusufjohan05@gmail.com>
2020-12-06 21:29:55 +0700

    CRUD Operation sudah siap

---
+++ IService1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CRUD_Operation
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string Insert(InsertUser value);
+
+        [OperationContract]
+        gettestdata GetInfo();
+
+        [OperationContract]
+        string Update(UpdateUser update);
+
+        [OperationContract]
+        string Delete(DeleteUser delete);
+
+//        [OperationContract]
+//        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class InsertUser
+    {
+        //        bool boolValue = true;
+        //        string stringValue = "Hello ";
+
+        string name = string.Empty;
+        string email = string.Empty;
+
+//        [DataMember]
+//        public bool BoolValue
+//        {
+//            get { return boolValue; }
+//            set { boolValue = value; }
+//        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class gettestdata
+    {
+        [DataMember]
+        public DataTable usertab
+        {
+            get;
+            set;
+        }
+    }
+
+    [DataContract]
+    public class UpdateUser
+    {
+        int uid;
+        string name;
+        string email;
+
+        [DataMember]
+        public int UID
+        {
+            get { return uid; }
+            set { uid = value; }
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+    }
+
+    [DataContract]
+    public class DeleteUser
+    {
+        int uid;
+
+        [DataMember]
+        public int UID
+        {
+            get { return uid; }
+            set { uid = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CRUD_Operation
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string Delete(DeleteUser delete)
+        {
+            string message = "";
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=CRUD_Operation;User ID=sa;Password=goldensilk2020");
+            connection.Open();
+            SqlCommand command = new SqlCommand("delete UserTab where UserID = @UserID", connection);
+            command.Parameters.AddWithValue("@UserID", delete.UID);
+            int res = command.ExecuteNonQuery();
+            if (res == 1)
+            {
+                message = "Data Berhasil Dihapus!";
+            }
+            else
+            {
+                message = "Data Tidak Berhasil Dihapus";
+            }
+            return message;
+        }
+
+        public gettestdata GetInfo()
+        {
+            gettestdata g = new gettestdata();
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=CRUD_Operation;User ID=sa;Password=goldensilk2020");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * from UserTab", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable("mytab");
+            dataAdapter.Fill(dataTable);
+            g.usertab = dataTable;
+            return g;
+        }
+
+        public string Insert(InsertUser user)
+        {
+            //return string.Format("You entered: {0}", value);
+
+            string msg;
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=CRUD_Operation;User ID=sa;Password=goldensilk2020");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Insert into UserTab (Name, Email) values(@Name, @Email)", connection);
+            command.Parameters.AddWithValue("@Name", user.Name);
+            command.Parameters.AddWithValue("@Email", user.Email);
+
+            int g = command.ExecuteNonQuery();
+            if(g==1)
+            {
+                msg = "Data Berhasil Dimasukkan!";
+            }
+            else
+            {
+                msg = "Data Tidak Berhasil Dimasukkan!";
+            }
+            return msg;
+        }
+
+        public string Update(UpdateUser update)
+        {
+            string Message = "";
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=CRUD_Operation;User ID=sa;Password=goldensilk2020");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Update UserTab set Name = @Name, Email = @Email where UserID= @UserID", connection);
+            command.Parameters.AddWithValue("@UserID", update.UID);
+            command.Parameters.AddWithValue("@Name", update.Name);
+            command.Parameters.AddWithValue("@Email", update.Email);
+            int res = command.ExecuteNonQuery();
+            if (res == 1)
+            {
+                Message = "Data Berhasil Diperbaharui!";
+            }
+            else
+            {
+                Message = "Data Tidak Berhasil Diperbaharui!";
+            }
+            return Message;
+        }
+
+        //        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //        {
+        //            if (composite == null)
+        //            {
+        //                throw new ArgumentNullException("composite");
+        //            }
+        //            if (composite.BoolValue)
+        //            {
+        //                composite.StringValue += "Suffix";
+        //            }
+        //            return composite;
+        //        }
+    }
+}
